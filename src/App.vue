2e825f0c6b8f6ca9df5c642bbfb3757ec2ee0be8
kae montoia <kae.sqta04b@hotmail.com>
2025-06-26 16:25:36 -0300

    first commit files vue3

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="cotainer">
+    <section>
+      <b-button type="is-primary">Primary</b-button>
+    </section>
+  </div>
+</template>
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+const app = createApp(App)
+app.use(Buefy)
+app.mount('#app')
